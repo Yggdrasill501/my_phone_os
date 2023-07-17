9858bae0cd0a0e349ec154ed4915fa9ea6a8b1e0
Filip Žitný <filipzitny@gmail.com>
2023-07-18 00:39:54 +0200

    kernel

--- kernel_start.h
+++ kernel_start.h
@@ -5,14 +5,15 @@
 #ifndef MY_PHONE_OS_KERNEL_START_H
 #define MY_PHONE_OS_KERNEL_START_H
 
-include "kernel_main.h"
+include "kernel.h"
 
 
 extern "C" void KernelStart(void){
     // Initialize hardware, set up memory, etc.
 
     // Call the main kernel function
-    kernel_main();
+    Kernel kernel;
+    kernel.start();
 }
 
 
