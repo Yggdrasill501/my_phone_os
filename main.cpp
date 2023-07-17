15dfeaaeba488a036bcbbf2a5e9ae7aa1dd3024b
Filip Žitný <filipzitny@gmail.com>
2023-07-18 00:48:58 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include "kernel.h"
+#include "kernel_start.h"
+#include "myapp.h"
+#include "touchscreendriver.h"
+#include "window.h"
+#include "button.h"
+#include "windowmanager.h"
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
